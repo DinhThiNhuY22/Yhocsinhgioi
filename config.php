76cab4b00e923c3d172e60f25907cbe56f085da4
DinhThiNhuY22 <ydangyeu247@gmail.com>
2026-01-02 01:38:19 +0700

    Up code clean

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define("host_name", "localhost");
+define("db_user", "root");
+define("db_password", "");
+define("db_name", "tracnghiemonline");
+define("app_path", "/qldethitracnghiem/");
+define("login_path", "http://localhost/qldethitracnghiem/auth/signin");
